db3abaef39f0176b1f28cd12fc81fdcb6f731cef
Karm Pandya <KarmPandya007@users.noreply.github.com>
2026-01-13 14:07:21 +0530

    Added the basic server details

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req, res)=>{
+    res.send("Server Running Successfully");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
