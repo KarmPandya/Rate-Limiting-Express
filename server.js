5a7ed7eef873a503c7f0c8bfd8de51cf43b23527
Karm Pandya <karmpandya14@gmail.com>
2026-01-14 03:01:58 +0530

    Optimized the code and fixed all the bugs

--- server.js
+++ server.js
@@ -3,47 +3,42 @@ import cors from "cors";
 import dotenv from "dotenv";
 import peopleRoutes from './routes/PeopleRoutes.js';
 import { connectDB } from "./config/db.js";
-import { rateLimit } from 'express-rate-limit'
+import { rateLimit } from 'express-rate-limit';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+// Middleware
+app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
 app.use(express.json());
 
+// Rate Limiter
 const limiter = rateLimit({
-  windowMs : 5 * 60 * 1000, // 5 mins
-  limit : 5,
-  message : "Too many requests, try again after some time"
-})
-
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // Maximum 5 requests
+  // if we hit the API for more than 5 times the error will be : 429 Too Many Requests
+  message: "Too many requests, try again later."
+});
 app.use(limiter);
 
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server running successfully 🚀",
+  });
+});
 
+app.use("/api/people", peopleRoutes);
+
+// Start Server
 const startServer = async () => {
   try {
     await connectDB();
-
-    app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
-
-    //  -------------------- Routes -------------------- 
-    app.get("/", (req, res) => {
-      res.status(200).json({
-        success: true,
-        message: "Server running successfully 🚀",
-      });
-      res.send("Server running successfully")
-    });
-
-    app.use("/api/people", peopleRoutes);
-
-    //  -------------------- Start Server -------------------- 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on http://localhost:${PORT}`);
     });
-
-
   } catch (error) {
     console.error("❌ Failed to start server:", error.message);
     process.exit(1);
