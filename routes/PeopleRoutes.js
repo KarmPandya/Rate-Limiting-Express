4e8414474ac487070517a6fd9b7573e509ccd22a
Karm Pandya <karmpandya14@gmail.com>
2026-01-18 13:52:51 +0530

    Optimized the code and did the required updates

--- routes/PeopleRoutes.js
+++ routes/PeopleRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { protect } from '../middlewares/authMiddleware.js'
 import { getAllPeople, getPersonById, updatePersonById, deletePersonById } from '../controllers/PeopleController.js'
 
 const router = express.Router()
 
-router.get('/', getAllPeople)
-router.get('/:id', getPersonById)
+router.get('/', protect, getAllPeople)
+router.get('/:id', protect, getPersonById)
 router.put('/:id', updatePersonById)
-router.delete('/:id', deletePersonById)
+router.delete('/:id', protect, deletePersonById)
 
 export default router;
